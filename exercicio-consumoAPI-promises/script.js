7d620e537288d4d80b97235e6fca12d18ac92183
Robson <robsonshockwavegithub@gmail.com>
2021-06-03 19:15:41 -0300

    consumo de api e promises

---
+++ exercicio-consumoAPI-promises/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    $("button").click(buscarInformacoes)
+});
+
+buscarCidade = function(cidade) {
+    return new Promise(function(resolve, reject) {
+        let resultado;
+        if(typeof cidade == 'string') {
+            resultado = $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=f32220c86dbf37908be0eb2721823e3b");
+        } else {
+            reject();
+        }
+        
+        if(resultado) {
+            resolve(resultado);
+        } else {
+            reject();
+        }
+    });
+}
+
+colocarNoHtml = function(indo) {
+    const climaAtual = indo.weather[0].description;
+    const temperaturaAtual = (indo.main.temp - 273.15).toFixed(2);
+    const temperaturaMinima = (indo.main.temp_min - 273.15).toFixed(2);
+    const temperaturaMaxima = (indo.main.temp_max - 273.15).toFixed(2);
+    
+    $("#resultado").html(
+        `<p>Condição Atual: ${climaAtual}</p>
+        <p>Temperatura Atual: ${temperaturaAtual}</p>
+        <p>Temperatura máxima: ${temperaturaMaxima}</p>
+        <p>Temperatura minima: ${temperaturaMinima}</p>
+        `
+    );
+}
+
+function buscarInformacoes() {
+    let cidade = $("input").val();
+
+    buscarCidade(cidade).then(colocarNoHtml).catch((err) => {
+        return $("#resultado").html(
+            `<p id="error">Cidade não encontrada</p>`
+        );
+    });
+}
